24751bc30fd51f582020b71150c7fdfbf243c785
gowthamjidagam <gowtham.jidagam@gmail.com>
2025-12-17 11:44:06 +0530

    Array - 334. Increasing Triplet Subsequence - Retrieve Indices

--- src/Arrays/IncTripSubSeq.py
+++ src/Arrays/IncTripSubSeq.py
@@ -44,3 +44,34 @@ class Solution(object):
             else:
                 return True
         return False
+
+    def increasingTriplet2(self, nums):
+        """
+        This will return actual indices
+        :param nums:
+        :return:
+        """
+        n = len(nums)
+        smallest = float("inf")
+        smallest_idx = -1
+        sec_smallest = float("inf")
+        sec_smallest_idx = -1
+        sec_small_idx = -1
+        for i in range(n):
+            if nums[i] <= smallest:
+                smallest = nums[i]
+                smallest_idx = i
+            elif nums[i] <= sec_smallest:
+                sec_smallest = nums[i]
+                sec_smallest_idx = i
+                sec_small_idx = smallest_idx
+            else:
+                return (sec_small_idx,sec_smallest_idx,i)
+
+nums = [2,1,5,0,4,6]
+nums = [5,4,3,2,1]
+nums = [1,2,3,4,5]
+nums = [10,20,3,2,1,1,2,0,4]
+sol = Solution()
+print(sol.increasingTriplet(nums))
+print(sol.increasingTriplet2(nums))
