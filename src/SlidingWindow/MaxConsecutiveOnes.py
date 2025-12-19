ca3bf9942aa5abd6474ab22a3be009c52add4d00
gowthamjidagam <gowtham.jidagam@gmail.com>
2025-12-19 09:59:01 +0530

    Sliding Window - 1004. Max Consecutive Ones III

---
+++ src/SlidingWindow/MaxConsecutiveOnes.py
@@ -0,0 +1,47 @@
+"""
+1004. Max Consecutive Ones III
+
+Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+Example 1:
+
+Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+Output: 6
+Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+Example 2:
+
+Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+Output: 10
+Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+"""
+
+class Solution(object):
+    def longestOnes(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        left = 0
+        right = 0
+        max_len = 0
+        curr = 0
+        while right < len(nums):
+            if nums[right] == 0:
+                curr += 1
+            while curr > k:
+                if nums[left] == 0:
+                    curr -= 1
+                left += 1
+            right += 1
+            max_len = max(max_len, right - left)
+        return max_len
+
+nums = [1,1,1,0,0,0,1,1,1,1,0]
+k = 2
+nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1]
+k = 3
+sol = Solution()
+print(sol.longestOnes(nums,k))
